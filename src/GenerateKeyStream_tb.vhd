5b8df97654273bdb55304b89230adf38834f71c6
brenojesus-ecomp <breno.jesus@ecomp.ufsm.br>
2026-06-07 19:10:48 -0300

    Update GenerateKeyStream_tb.vhd
    Mudei o clock para 1ns para a frequência ser maior que o data_av

--- src/GenerateKeyStream_tb.vhd
+++ src/GenerateKeyStream_tb.vhd
@@ -34,7 +34,7 @@ begin
     -- espera-se que a saída seja 09 04 07 03 00 08 06 01 05 02 09 05 07 02 04
     -- tudo isso considerando hexadecimal
     rst <= '1', '0' after 3 ns;
-    clk <= not clk after 5 ns;
+    clk <= not clk after 1 ns;
     data <= "00000000", "00001010" after 9 ns, "00000101" after 19 ns, "00001010" after 29 ns;
     data_av <= '0', '1' after 4 ns, '0' after 9 ns, '1' after 14 ns, '0' after 19 ns, '1' after 24 ns, '0' after 29 ns, '1' after 34 ns, '0' after 39 ns;    
 end behavioral;
