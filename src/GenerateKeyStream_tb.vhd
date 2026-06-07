c33c588e22f1836726d437e52feb553e26b307d0
estevao-all <135400853+estevao-all@users.noreply.github.com>
2026-06-07 20:14:44 -0300

    aaaaaaaa

--- src/GenerateKeyStream_tb.vhd
+++ src/GenerateKeyStream_tb.vhd
@@ -37,6 +37,24 @@ begin
     clk <= not clk after 1 ns;
     data <= "00000000", "00001010" after 9 ns, "00000101" after 19 ns, "00001010" after 29 ns;
     data_av <= '0', '1' after 4 ns, '0' after 9 ns, '1' after 14 ns, '0' after 19 ns, '1' after 24 ns, '0' after 29 ns, '1' after 34 ns, '0' after 39 ns;
+
+    GENERATE_KEY_STREAM_A: entity work.GenerateKeyStream(behavioral)
+		port map (
+			clk		    => clk,
+			rst		    => rst,
+            data_av     => data_av,
+            done        => done,
+            data        => Data
+        );
+
+    -- na memória deve estar
+    -- 00 06 01 04 09 03 08 07 05 02
+    -- espera-se que a saída seja 09 04 07 03 00 08 06 01 05 02 09 05 07 02 04
+    -- tudo isso considerando hexadecimal
+    rst <= '1', '0' after 3 ns;
+    clk <= not clk after 1 ns;
+    data <= "00000000", "00001010" after 9 ns, "00000101" after 19 ns, "00001010" after 29 ns;
+    data_av <= '0', '1' after 4 ns, '0' after 9 ns, '1' after 14 ns, '0' after 19 ns, '1' after 24 ns, '0' after 29 ns, '1' after 34 ns, '0' after 39 ns;
 end behavioral;
 
 
